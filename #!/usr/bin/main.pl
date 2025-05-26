7d60f7a7b1b3d8ebe0e9a9e237a17d63f851d2ab
Yuji <166590039+uj13e@users.noreply.github.com>
2025-05-26 10:36:32 +0900

    Delete #!/usr/bin directory

--- #!/usr/bin/main.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-@tensuu=(30,60,70,20,60);
-@namae=("Taro","Jiro","Sabro","shiro","Goro");
-$a=0;
-for($i=0;$i<5;$i++){
-	print("$i: @namae[$i]の点数は@tensuu[$i]点です\n");
-	$a+=@tensuu[$i];
-}
-print("平均点は",$a/5,"\n");
